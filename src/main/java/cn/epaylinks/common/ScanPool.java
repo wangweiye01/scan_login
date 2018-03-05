dc92d02232d4baf5046417aedd3e9e0da5afb31e
wangweiye <wwyknight@163.com>
2018-03-05 16:13:39 +0800

    fix

--- src/main/java/cn/epaylinks/common/ScanPool.java
+++ src/main/java/cn/epaylinks/common/ScanPool.java
@@ -1,79 +1,64 @@
 package cn.epaylinks.common;
 
-public class ScanPool
-{
+public class ScanPool {
 
-	//创建时间
-	private Long createTime = System.currentTimeMillis();
-	
-	//登录状态
-	private boolean scanFlag = false;
-	
-	public boolean isScan(){
-		return scanFlag;
-	}
-	
-	public void setScan(boolean scanFlag){
-		this.scanFlag = scanFlag;
-	}
-	
-	/**
-	 * 获取扫描状态，如果还没有扫描，则等待固定秒数
-	 * @param wiatSecond 需要等待的秒数
-	 * @return
-	 */
-	public synchronized boolean getScanStatus(){
-		try
-		{
-			if(!isScan()){ //如果还未扫描，则等待
-				this.wait();
-			}
-			if (isScan())
-			{
-				return true;
-			}
-		} catch (InterruptedException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
-	
-	/**
-	 * 扫码之后设置扫码状态
-	 */
-	public synchronized void scanSuccess(){
-		try
-		{
-			setScan(true);
-			this.notifyAll();
-		} catch (Exception e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public synchronized void notifyPool(){
-		try
-		{
-			this.notifyAll();
-		} catch (Exception e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    //创建时间
+    private Long createTime = System.currentTimeMillis();
 
-	public Long getCreateTime()
-	{
-		return createTime;
-	}
+    //登录状态
+    private boolean scanFlag = false;
 
-	public void setCreateTime(Long createTime)
-	{
-		this.createTime = createTime;
-	}
+    public boolean isScan() {
+        return scanFlag;
+    }
+
+    public void setScan(boolean scanFlag) {
+        this.scanFlag = scanFlag;
+    }
+
+    public synchronized boolean getScanStatus() {
+        try {
+            if (!isScan()) { //如果还未扫描，则等待
+                this.wait();
+            }
+            if (isScan()) {
+                return true;
+            }
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 扫码之后设置扫码状态
+     */
+    public synchronized void scanSuccess() {
+        try {
+            setScan(true);
+            this.notifyAll();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void notifyPool() {
+        try {
+            this.notifyAll();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
 
 }
--- src/main/java/cn/epaylinks/controller/UserController.java
+++ src/main/java/cn/epaylinks/controller/UserController.java
@@ -43,125 +43,116 @@ import cn.epaylinks.service.IUserService;
 
 @Controller
 @EnableAutoConfiguration
-public class UserController
-{
-	private Logger logger = Logger.getLogger(UserController.class);
+public class UserController {
+    private Logger logger = Logger.getLogger(UserController.class);
 
-	@Autowired
-	private IUserService userService;
-	
-	@RequestMapping("/userlist")
-	@ResponseBody
-	ResponseEntity<List<User>> userList()
-	{
-		logger.info("进入Controller，查询用户list");
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.TEXT_PLAIN);
-		List<User> list = userService.getUserList();
-		return new ResponseEntity<List<User>>(list, headers, HttpStatus.OK);
-	}
-	
-	@RequestMapping("/qrcode/{uuid}")
-	@ResponseBody
-	String createQRCode(@PathVariable String uuid,HttpServletResponse response)
-	{
-		System.out.println("生成二维码");
-		
-		String text = "http://2b082e46.ngrok.io/login/"+uuid;
-		int width = 300;   
-		int height = 300;   
-		String format = "png";   
-		//将UUID放入缓存
-		ScanPool pool = new ScanPool();
-		PoolCache.cacheMap.put(uuid, pool);
-		try
-		{
-			Map<EncodeHintType, Object> hints= new HashMap<EncodeHintType, Object>();   
-			hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
-			//hints.put(EncodeHintType.MARGIN, 1);
-			hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H); //容错率
-			BitMatrix bitMatrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, width, height,hints);
-			MatrixToImageWriter.writeToStream(bitMatrix, format, response.getOutputStream());
-		} catch (WriterException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	@RequestMapping("/login/{uuid}")
-	@ResponseBody
-	String login(@PathVariable String uuid){
-		
-		ScanPool pool = PoolCache.cacheMap.get(uuid);
-		
-		if(pool == null){
-			return "timeout,scan fail";
-		}
-		
-		pool.scanSuccess();
-		
-		return "扫码完成，登录成功";
-	}
-	
-	@RequestMapping("/pool")
-	@ResponseBody
-	String pool(String uuid){
-		System.out.println("检测["+uuid+"]是否登录");
-		
-		ScanPool pool = PoolCache.cacheMap.get(uuid);
-		
-		if(pool == null){
-			return "timeout";
-		}
-		
-		//使用计时器，固定时间后不再等待扫描结果--防止页面访问超时
-		new Thread(new ScanCounter(uuid)).start();
-		
-		boolean scanFlag = pool.getScanStatus();
-		if(scanFlag){
-			return "success";
-		}else{
-			return "fail";
-		}
-	}
-	
-	class ScanCounter implements Runnable{
-		
-		public Long timeout = 27000L;
-		
-		//传入的对象
-		private String uuid;
-		
-		public ScanCounter(String p){
-			uuid = p;
-		}
+    @Autowired
+    private IUserService userService;
 
-		@Override
-		public void run()
-		{
-			// TODO Auto-generated method stub
-			try
-			{
-				Thread.sleep(timeout);
-			} catch (InterruptedException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			notifyPool(uuid);
-		}
-		
-		public synchronized void notifyPool(String uuid){
-			ScanPool pool = PoolCache.cacheMap.get(uuid);
-			pool.notifyPool();
-		}
-		
-	}
-	
+    @RequestMapping("/userlist")
+    @ResponseBody
+    ResponseEntity<List<User>> userList() {
+        logger.info("进入Controller，查询用户list");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        List<User> list = userService.getUserList();
+        return new ResponseEntity<List<User>>(list, headers, HttpStatus.OK);
+    }
+
+    @RequestMapping("/qrcode/{uuid}")
+    @ResponseBody
+    String createQRCode(@PathVariable String uuid, HttpServletResponse response) {
+        System.out.println("生成二维码");
+
+        String text = "http://2b082e46.ngrok.io/login/" + uuid;
+        int width = 300;
+        int height = 300;
+        String format = "png";
+        //将UUID放入缓存
+        ScanPool pool = new ScanPool();
+        PoolCache.cacheMap.put(uuid, pool);
+        try {
+            Map<EncodeHintType, Object> hints = new HashMap<EncodeHintType, Object>();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+            //hints.put(EncodeHintType.MARGIN, 1);
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H); //容错率
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, width, height, hints);
+            MatrixToImageWriter.writeToStream(bitMatrix, format, response.getOutputStream());
+        } catch (WriterException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping("/login/{uuid}")
+    @ResponseBody
+    String login(@PathVariable String uuid) {
+
+        ScanPool pool = PoolCache.cacheMap.get(uuid);
+
+        if (pool == null) {
+            return "timeout,scan fail";
+        }
+
+        pool.scanSuccess();
+
+        return "扫码完成，登录成功";
+    }
+
+    @RequestMapping("/pool")
+    @ResponseBody
+    String pool(String uuid) {
+        System.out.println("检测[" + uuid + "]是否登录");
+
+        ScanPool pool = PoolCache.cacheMap.get(uuid);
+
+        if (pool == null) {
+            return "timeout";
+        }
+
+        //使用计时器，固定时间后不再等待扫描结果--防止页面访问超时
+        new Thread(new ScanCounter(uuid)).start();
+
+        boolean scanFlag = pool.getScanStatus();
+        if (scanFlag) {
+            return "success";
+        } else {
+            return "fail";
+        }
+    }
+
+    class ScanCounter implements Runnable {
+
+        public Long timeout = 27000L;
+
+        //传入的对象
+        private String uuid;
+
+        public ScanCounter(String p) {
+            uuid = p;
+        }
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            try {
+                Thread.sleep(timeout);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            notifyPool(uuid);
+        }
+
+        public synchronized void notifyPool(String uuid) {
+            ScanPool pool = PoolCache.cacheMap.get(uuid);
+            pool.notifyPool();
+        }
+
+    }
+
 }
